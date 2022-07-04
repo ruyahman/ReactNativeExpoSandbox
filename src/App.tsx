9455e26a8c3cd0b02842335e46205edca39fc6d6
t-haruyama <ht94520791701.tuka@gmail.com>
2022-07-04 10:57:28 +0900

    Header追加 safeArea考慮

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {RootStackNav} from 'navigation';
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import { WebView } from 'react-native-webview';
 
+import { Header } from 'react-native-elements'
+import { View, ActivityIndicator } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
 export const App = () => {
+  loading = () => {
+    return <ActivityIndicator style={{ flex: 1, bottom: "30%" }}
+                              color='hsla(0, 100%, 50%, 0.9)'
+                              size='large' />
+  }
+
   return (
-    <WebView
-      source={{ uri: 'https://www.google.com/' }}
-    />
+    <SafeAreaView style={{ flex: 1 }}>
+        <Header
+          centerComponent={{
+            text: 'WebView',
+            style: {
+              color: 'lightblue',
+              fontSize: 24,
+              fontFamily: 'Baskerville-Bold',
+            }
+          }}
+          leftComponent={
+            <Icon
+              name="chevron-left"
+              size={20} color="white"
+              style={{ marginLeft: 5 }}
+              onPress={() => this.webView.goBack()}
+            />
+          }
+          rightComponent={
+            <Icon
+              name="chevron-right"
+              size={20}
+              color="white"
+              style={{ marginRight: 5 }}
+              onPress={() => this.webView.goForward()}
+            />
+          }
+          backgroundColor="black"
+          barStyle="light-content"
+        />
+        <WebView
+          ref={ref => this.webView = ref}
+          source={{ uri: 'https://www.google.com' }}
+          renderLoading={() => this.loading()}
+          startInLoadingState={true}
+        />
+      </SafeAreaView>
   );
 };
